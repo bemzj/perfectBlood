0b9f42e08a6a757cdb22dd52c3e83096fd9166e5
bemzj <qiuzijia520@qq.com>
2017-11-07 15:56:10 +0800

    xx

--- js/resource.js
+++ js/resource.js
@@ -13,7 +13,7 @@ var gameImg = [
 	{path:'img/lifes.png?V=2',type:'img',name:'lifes'},//ta
 	{path:'img/bheart.png',type:'img',name:'bheart'},//心
 	{path:'img/music.png',type:'img',name:'music'},//音乐
-	{path:'img/logo.png?v=2',type:'img',name:'logo'},//完美logo
+	{path:'img/logo.png?v=3',type:'img',name:'logo'},//完美logo
 	{path:'img/hand.png',type:'img',name:'hand'},//手
 	{path:'img/hand2.png',type:'img',name:'hand2'},//手
 	{path:'img/person.png',type:'img',name:'person'},//人
@@ -57,7 +57,7 @@ var gameImg = [
 	{path:'img/share.png?v=2',type:'img',name:'share'},//分享
 	{path:'img/shareTitle.png?v=2',type:'img',name:'shareTitle'},//分享语
 	{path:'img/transition.jpg?v=2',type:'img',name:'transition'},//过度页面
-	{path:'img/lastBkg.jpg?V=2',type:'img',name:'lastBkg'},//背景
+	{path:'img/lastBkg.jpg?V=3',type:'img',name:'lastBkg'},//背景
 	{path:'img/c1.png',type:'img',name:'c1'},//c1
 	{path:'img/c2.png',type:'img',name:'c2'},//c2
 	{path:'img/c3.png',type:'img',name:'c3'},//c3
